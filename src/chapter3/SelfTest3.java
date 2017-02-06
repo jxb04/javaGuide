764f2333a1e3c99e74ee4d23a35f1d9f1818bc8d
Joel Bernasky <joel_bernasky@hotmail.com>
2017-02-06 15:42:34 -0500

    adding chapter 3 self test

---
+++ src/chapter3/SelfTest3.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+public class SelfTest3 {
+
+	public static void main(String args[]) throws java.io.IOException {
+		
+		char ignore;
+		int spaceCount = 0;
+		
+		System.out.println("Enter a period to stop.");
+		do {
+			ignore = (char) System.in.read();
+			if (ignore == ' ') {
+				spaceCount = spaceCount + 1;
+			}
+		}  while (ignore != '.');
+		
+		System.out.println("The number of spaces that were input is: " + spaceCount);
+	}
+	
+}
