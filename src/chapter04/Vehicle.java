62ab4e6546d077f98c4f7745f67a1c48db114016
Joel Bernasky <joel_bernasky@hotmail.com>
2017-02-08 11:22:08 -0500

    implementation of range method

--- src/chapter04/Vehicle.java
+++ src/chapter04/Vehicle.java
@@ -4,4 +4,9 @@ public class Vehicle {
 	int passengers; // number of passengers
 	int fuelcap;    // fuel capacity in gallons
 	int mpg;        // fuel consumption in miles per gallon
+	
+	//Display the range
+	int range() {
+		return mpg * fuelcap;
+	}
 }
