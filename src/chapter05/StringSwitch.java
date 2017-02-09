4585da80f6682a1008315b62b55e77de6eb70804
Joel Bernasky <joel_bernasky@hotmail.com>
2017-02-09 16:24:54 -0500

    string manipulation

---
+++ src/chapter05/StringSwitch.java
@@ -0,0 +1,26 @@
+package chapter05;
+
+// Use a string to control a switch statement.
+
+public class StringSwitch {
+	public static void main (String args[]) {
+
+		String command = "cancel";
+		
+		switch (command) {
+		case "connect" :
+			System.out.println("Connecting");
+			break;
+		case "cancel" :
+			System.out.println("Cancelling");
+			break;
+		case "disconnect" :
+			System.out.println("Disconnecting");
+			break;
+		default :
+			System.out.println("Command Error!");
+			break;
+		}
+	}
+
+}
