9a3ac87172f00f9a0ce2868e56b2b9eef762d6f9
Joel Bernasky <joel_bernasky@hotmail.com>
2017-02-15 10:37:17 -0500

    overloading constructors

---
+++ src/chapter06/OverloadConsDemo.java
@@ -0,0 +1,42 @@
+package chapter06;
+
+// Demonstrates an overloaded constructor
+class MyClass2 {
+	int x;
+	
+	MyClass2() {
+		System.out.println("Inside MyClass2().");
+		x = 0;
+	}
+	
+	MyClass2(int i){
+		System.out.println("Inside MyClass2(int).");
+		x = i;
+	}
+	
+	MyClass2(double d) {
+		System.out.println("Inside MyClass2(double).");
+		x = (int) d;
+	}
+	
+	MyClass2(int i, int j) {
+		System.out.println("Inside MyClass2(int, int)");
+		x = i + j;
+	}
+}
+
+public class OverloadConsDemo {
+	public static void main(String args[]) {
+		MyClass2 t1 = new MyClass2();
+		MyClass2 t2 = new MyClass2(88);
+		MyClass2 t3 = new MyClass2(17.23);
+		MyClass2 t4 = new MyClass2(2, 4);
+		
+		System.out.println("t1.x: " + t1.x);
+		System.out.println("t2.x: " + t2.x);
+		System.out.println("t3.x: " + t3.x);
+		System.out.println("t4.x: " + t4.x);
+		
+	}
+
+}
---
+++ src/chapter06/QDemo2.java
@@ -0,0 +1,102 @@
+package chapter06;
+
+// A queue class for characters.  Using Queue2 as the class name since Queue is already used in this package.
+class Queue2 {
+	private char q[];  // this array holds the queue
+	private int putloc, getloc; // the put and get indicies
+	
+	// Construct an empty Queue given its size
+	Queue2 (int size) {
+		q = new char[size]; // allocate memory for queue
+		putloc = getloc = 0;
+	}
+	
+	// Construct a Queue from a Queue.
+	Queue2(Queue2 ob) {
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+	}
+	
+	// Construct a Queue with initial values.
+	Queue2(char a[]) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+		
+		for (int i = 0; i < a.length; i++) put(a[i]);
+	}
+	
+	// Put a character in the queue.
+	void put(char ch) {
+		if(putloc==q.length) {
+			System.out.println(" - Queue is full.");
+			return;
+		}
+		
+		q[putloc++] = ch;
+	}
+	
+	// Get a character from the queue
+	char get() {
+		if (getloc == putloc) {
+			System.out.println(" - Queue is empty.");
+			return (char) 0;
+		}
+		return q[getloc++];
+	}
+}
+
+public class QDemo2 {
+	public static void main(String args[]) {
+		// construct 10-element empty queue
+		Queue2 q1 = new Queue2(10);
+		
+		char name[] = {'T', 'o', 'm'};
+		// construct queue from array
+		Queue2 q2 = new Queue2(name);
+		
+		char ch;
+		int i;
+		
+		// put some characters into q1
+		for (i = 0; i < 10; i++)
+			q1.put((char) ('A' + i));
+		
+		System.out.print("First contents of q1: ");
+/*		for (i = 0; i < 10; i++) {
+			ch = q1.get();
+			System.out.print(ch);
+		}
+*/		System.out.println('\n');
+
+		// construct queue from another queue
+		Queue2 q3 = new Queue2(q1);
+		
+		
+		// Show the queues
+		System.out.print("Contents of q1: ");
+		for (i = 0; i < 10; i++) {
+			ch = q1.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println('\n');
+		
+		System.out.print("Contents of q2: ");
+		for (i = 0; i < 3; i++) {
+			ch = q2.get();
+			System.out.print(ch);
+		}
+		
+		System.out.println('\n');
+		
+		System.out.print("Contents of q3: ");
+		for(i = 0; i < 10; i++) {
+			ch = q3.get();
+			System.out.print(ch);
+		}
+		
+	}
+
+}
