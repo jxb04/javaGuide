41b9a2532fb403171760064f3f3788b55e654bd8
Joel Bernasky <joel_bernasky@hotmail.com>
2017-03-03 15:57:55 -0500

    multiple implementations of same interface

---
+++ src/chapter08/ICharQ.java
@@ -0,0 +1,11 @@
+package chapter08;
+
+// A character queue interface
+public interface ICharQ {
+	// put a character in the queue
+	void put(char ch);
+	
+	// get a character from the queue
+	char get();
+
+}
