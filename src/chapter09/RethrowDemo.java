34270520499d7f51a7fd1794e10de754dd6dc747
Joel Bernasky <joel_bernasky@hotmail.com>
2017-03-08 16:03:57 -0500

    rethrowing an exception

---
+++ src/chapter09/RethrowDemo.java
@@ -0,0 +1,40 @@
+package chapter09;
+
+// rethrow an exception
+class Rethrow {
+	public static void genException() {
+		// here numer is longer than denom
+		int numer[] = {4, 8, 16, 32, 64, 128, 512, 1024};
+		int denom[] = {2, 0, 4, 4, 0, 8};
+		
+		for (int i = 0; i <= numer.length; i++) {
+			try {
+				System.out.println(numer[i] + " / " + denom[i] + " is " +
+			                       numer[i]/denom[i]);
+			}
+			catch (ArithmeticException exc) {
+				// catch the exception
+				System.out.println("Can't divide by zero.");
+			}
+			catch (ArrayIndexOutOfBoundsException exc) {
+				// catch the exception
+				System.out.println("No matching element found.");
+				throw exc;  // rethrow exception
+			}
+		}
+	}
+
+}
+
+public class RethrowDemo {
+	public static void main(String args[]) {
+		try {
+			Rethrow.genException();
+		}
+		catch (ArrayIndexOutOfBoundsException exc) {
+			// recatch exception
+			System.out.println("Fatal error - " + "program terminated.");
+		}
+	}
+
+}
