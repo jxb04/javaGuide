7b7ab62be161bea38c2e7be08cc2d9767374a872
Joel Bernasky <joel_bernasky@hotmail.com>
2017-03-10 08:55:10 -0500

    java i/o

---
+++ src/chapter10/WriteDemo.java
@@ -0,0 +1,13 @@
+package chapter10;
+
+// Demonstrate System.out.write().
+public class WriteDemo {
+	public static void main(String args[]) {
+		int b;
+		
+		b = 'X';
+		System.out.write(b);
+		System.out.write('\n');
+	}
+
+}
