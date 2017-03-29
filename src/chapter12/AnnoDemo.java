e3e290f3f02a55964d2707c4c1a39cda6b90aa6f
Joel Bernasky <joel_bernasky@hotmail.com>
2017-03-29 13:54:44 -0400

    annotations

---
+++ src/chapter12/AnnoDemo.java
@@ -0,0 +1,28 @@
+package chapter12;
+
+// An example that uses @Deprecated
+
+// Depracate a class.
+@Deprecated
+class MyClass {
+	private String msg;
+	
+	MyClass(String m) {
+		msg = m;
+	}
+	
+	// Deprecate a method within a class
+	@Deprecated
+	String getMsg() {
+		return msg;
+	}
+}
+
+public class AnnoDemo {
+	public static void main(String args[]) {
+		MyClass myObj = new MyClass("test");
+		
+		System.out.println(myObj.getMsg());
+	}
+
+}
