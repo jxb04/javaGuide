0e486a2488ab0c617135f0f8e8e98b9432e1f4a5
Joel Bernasky <joel_bernasky@hotmail.com>
2017-04-11 10:47:21 -0400

    full stack exception class

---
+++ src/chapter13/StackFullException.java
@@ -0,0 +1,12 @@
+package chapter13;
+
+public class StackFullException extends Exception {
+	int size;
+	
+	StackFullException(int s) { size = s; }
+	
+	public String toString() {
+		return "\nStack is full.  Maximum size is " + size;
+	}
+
+}
