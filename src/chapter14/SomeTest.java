a63e7d2aa1f4c9e1092ab72121a4547cca30be3a
Joel Bernasky <joel_bernasky@hotmail.com>
2017-04-13 13:45:22 -0400

    generic interface

---
+++ src/chapter14/SomeTest.java
@@ -0,0 +1,6 @@
+package chapter14;
+
+// A generic functional interface with two parameters that returns a boolean result.
+public interface SomeTest<T> {                // A generic functional interface
+	boolean test(T n, T m);
+}
