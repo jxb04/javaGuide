3699fa116ef139d566324bbbb347d996d24ca787
Joel Bernasky <joel_bernasky@hotmail.com>
2017-04-20 14:14:18 -0400

    block lambda

---
+++ src/chapter14/FactorialLambdaDemo.java
@@ -0,0 +1,19 @@
+package chapter14;
+
+public class FactorialLambdaDemo {
+	public static void main(String args[]) {
+		// This block lambda computes the factorial of an int value.
+		NumericFunc factorial = (n) -> {
+			int result = 1;
+			
+			for (int i = 1; i <= n; i++) {
+				result = i * result;
+			}
+			return result;
+		};
+		System.out.println("The factorial of 3 is " + factorial.func(3));
+		System.out.println("The factorial of 5 is " + factorial.func(5));
+		System.out.println("The factorial of 15 is " + factorial.func(15));
+	}
+
+}
