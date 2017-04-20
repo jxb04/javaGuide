794cd831c74d60e1bce0c493803ee7041c163a53
Joel Bernasky <joel_bernasky@hotmail.com>
2017-04-20 10:10:24 -0400

    separated out MyIntNum class

--- src/chapter14/MethodRefDemo2.java
+++ src/chapter14/MethodRefDemo2.java
@@ -1,18 +1,6 @@
 package chapter14;
 
-// This class stores an int value and defines the instance method isFactor(),
-// which returns true if its argument is a factor of the stored value.
-class MyIntNum {
-	private int v;
-	
-	MyIntNum(int x) { v = x; }
-	int getNum() { return v; }
-	
-	// Return true if n is a factor of v.
-	boolean isFactor (int n) {
-		return (v % n) == 0;
-	}
-}
+
 
 public class MethodRefDemo2 {
 	public static void main (String args[]) {
