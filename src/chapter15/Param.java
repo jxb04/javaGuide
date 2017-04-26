133bae478a687399e9f61b1cd30d1de1e3f27da3
Joel Bernasky <joel_bernasky@hotmail.com>
2017-04-26 10:29:44 -0400

    passing parameters to applets

---
+++ src/chapter15/Param.java
@@ -0,0 +1,43 @@
+package chapter15;
+// Pass a parameter to an applet.
+import java.applet.*;
+import java.awt.*;
+/*
+<applet code="chapter15.Param" width=300 height=80>
+<param name=author value="Herb Schildt">
+<param name=purpose value="Demonstrate Parameters">
+<param name=version value=2>
+</applet>
+ */
+public class Param extends Applet {
+	String author;
+	String purpose;
+	int ver;
+	
+	public void start() {
+		String temp;
+		
+		author = getParameter("author");
+		if(author == null) author = "not found";    // check that the parameter exists
+		
+		purpose = getParameter("purpose");
+		if (purpose == null) purpose = "not found";
+		
+		temp = getParameter("version");
+		try {
+			if (temp != null)
+				ver = Integer.parseInt(temp);
+			else
+				ver = 0;
+		} catch (NumberFormatException exc) {
+			ver = -1;    // error code
+		}
+	}
+	
+	public void paint(Graphics g) {
+		g.drawString("Purpose: " + purpose, 10, 20);
+		g.drawString("By: " + author, 10, 40);
+		g.drawString("Version: " + ver, 10, 60);
+	}
+
+}
